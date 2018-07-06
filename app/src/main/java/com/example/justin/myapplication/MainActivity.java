1d72ba022141535022e89ba0b8fea569d8a955fc
JustinLilly <39202473+JustinLilly@users.noreply.github.com>
2018-07-05 19:14:40 -0600

    minor changes

--- app/src/main/java/com/example/justin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/justin/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.justin.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(this, CreateValuation.class);
+        startActivity(i);
+
     }
 
     // I am testing!!!
